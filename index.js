4f4ae55a0c60b23abe02b4edec9cf1d4918484a4
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-08-10 12:57:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var app = require("express")();
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hello from BotsCube Backend"
+  });
+});
+app.listen(3000, () => {
+  console.log("Started");
+});
