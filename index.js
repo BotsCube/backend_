2fbb4f0ebb00368e25e2a3441651d10d4fa4b960
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-08-12 11:10:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,23 @@ const cors = require('cors');
 
 var data = {};
 
-const jwt = require('jsonwebtoken');
-
 const authenticateUser = (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(401).send('Not authenticated');
+      return res.status(401).json({ success: false, authenticated: false, error_message: `Not authenticated!`});
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!data[decoded.id]) {
-      return res.status(401).send('Not authenticated');
+      return res.status(401).json({ success: false, authenticated: false, error_message: `Not authenticated!`});
     }
 
     req.user = data[decoded.id];
     next();
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, });
+    res.status(500).json({ success: false, authenticated: false, error_message: `Profile not found!`});
   }
 };
 
