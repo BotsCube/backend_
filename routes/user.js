141e267d343b927fb09bc86a68749bbbd0c19158
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-08-16 16:51:05 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
-let router = require('express').Router;
+let express = require('express');
+let router = express.Router;
 
 router.get('/:id', async(req, res) => {
   let term = req.params.id;
   console.log(term);
   console.log(typeof term);
+
+  res.send(term + '||' + typeof term)
 })
 
 module.exports = router;
