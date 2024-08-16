5cdb660f0050b7916795f61aaa2db8e28a994da6
duplicate2991 <99782847+duplicate2991@users.noreply.github.com>
2024-08-16 18:47:26 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -9,7 +9,35 @@ let userData = [
   }
   ];
 router.get('/:id', async(req, res) => {
-  let userId = req.params.id;
+  try {
+    let userId = req.params.id;
+    if(isNaN(userId)){
+      return res.json({
+        success: false,
+        type: "invalid userid",
+        message: "An Invalid ID Is Provided!",
+        isNaN: isNaN(userId)
+      });
+    }
+    let userDetails = userData.filter(i => i.id === Number(userId));
+    if(!userDetails || typeof userDetails !== array || userDetails.length < 1){
+      return res.json({
+        success: false,
+        type: "account not found",
+        message: "No Account Found With This UserId",
+        isNaN: isNaN(userId)
+      });
+    }
+    res.json(userDetails[0]);
+  } catch (er) {
+    console.log(er);
+    return res.json({
+        success: false,
+        type: "server error",
+        message: "An Unknown Error Occurred!",
+        isNaN: undefined 
+      });
+  }
 })
 
 module.exports = router;
