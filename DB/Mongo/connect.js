fc2485038bf51dccf77e5279dd433dd516091e00
AnirudhSinh <jadejaanirudhsinh121@gmail.com>
2024-09-25 14:20:58 +0530

    Update connect.js

--- DB/Mongo/connect.js
+++ DB/Mongo/connect.js
@@ -2,20 +2,22 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 
 async function connectMDB() {
-  return new Promise((res) => {
-    mongoose.connect(process.env["MONGO_DB_URL"], {
+  if (!process.env["MONGO_DB_URL"]) {
+    console.error("MONGO_DB_URL environment variable is not defined");
+    return false;
+  }
+
+  try {
+    await mongoose.connect(process.env["MONGO_DB_URL"], {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-      .then(() => {
-        console.log(`Connected MongoDB`);
-        res(true);
-      })
-      .catch((err) => {
-        console.log(err);
-        res(false); // Optionally reject the promise on error
-      });
-  });
+    });
+    console.log("Connected MongoDB");
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 module.exports = connectMDB;
